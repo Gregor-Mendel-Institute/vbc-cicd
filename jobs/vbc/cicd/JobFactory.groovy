eed58ae8770de20c41963a596d7c860c974928b8
Erich Birngruber <erich.birngruber@gmi.oeaw.ac.at>
2019-08-16 15:35:05 +0200

    make sure permissions are seen

--- jobs/vbc/cicd/JobFactory.groovy
+++ jobs/vbc/cicd/JobFactory.groovy
@@ -187,6 +187,7 @@ class JobFactory {
             description(this.description)
 
             authorization {
+                assert this.permissionSets.size() > 0
                 for (perm in this.permissionSets) {
                     permissions(perm.key, perm.value)
                 }
