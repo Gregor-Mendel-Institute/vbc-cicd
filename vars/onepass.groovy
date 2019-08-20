b512ed658735d25854e28caa22081e37ec2af710
Erich Birngruber <erich.birngruber@gmi.oeaw.ac.at>
2019-08-20 17:02:04 +0200

    remove env call

--- vars/onepass.groovy
+++ vars/onepass.groovy
@@ -52,25 +52,27 @@
 }
  */
 
-
 def signin(String credentialsUsernamePassword, String credentialsDomainMasterKey) {
     echo "signing in to 1Password using credentials: ${credentialsUsernamePassword} and ${credentialsDomainMasterKey}"
 
     def onePassCredentials = [
         usernamePassword(credentialsId: credentialsUsernamePassword, usernameVariable: "OP_USERNAME", passwordVariable: "OP_PASSWORD"),
-        usernamePassword(credentialsId: credentialsDomainMasterKey, usernameVariable: "OP_DOMAIN", passwordVariable: "OP_DOMAIN_MASTER_KEY")
+        usernamePassword(credentialsId: credentialsDomainMasterKey, usernameVariable: "OP_DOMAIN", passwordVariable: "OP_MASTER_KEY")
     ]
 
     def onepass_token = null
     withCredentials(onePassCredentials) {
         echo "onepass env:"
-        sh "env"
+        // sh "env"
         echo "will sign in to domain: ${env.OP_DOMAIN}"
-        // usage: op signin <signinaddress> <emailaddress> <secretkey> [--output=raw]
-        onepass_token = sh label: "onepass", script: 'echo $OP_PASSWORD | op signin $OP_DOMAIN $OP_USERNAME $OP_MASTER_KEY --output=raw', returnStdout: true
 
-        // FIXME this is leaking
-        echo "got the token: ${onepass_token}"
+        // usage: op signin <signinaddress> <emailaddress> <secretkey> [--output=raw]
+        def signin_script = 'echo $OP_PASSWORD | op signin $OP_DOMAIN $OP_USERNAME $OP_MASTER_KEY --output=raw'
+
+        onepass_token = sh label: "onepass", script: signin_script, returnStdout: true
+
+        // FIXME remove it, this is leaking, was: for debugging
+        // echo "got the token: ${onepass_token}"
 
         if (onepass_token == null) {
             error('failed to retrieve 1Password session token')
@@ -83,6 +85,25 @@ def signin(String credentialsUsernamePassword, String credentialsDomainMasterKey
 
 def lookup(String itemName, String vault=null, String section='default', String field = 'password') {
 
+    Map raw = raw(itemName, vault)
+
+}
+
+// groovy method caching
+@Memoized
+def raw(String itemName, String vault = null) {
+
+    //def cached_item = itemCache.get(itemName)
+    //if (cached_item)
+    //    return cached_item
+
+    def vault_param = vault ? "--vault=${vault}" : ""
+    def item_raw = sh label: "onepass", script: "op get item ${itemName} ${vault_param}", returnStdout: true
+
+    Map item_data = readJson text: item_raw
+
+    //itemCache[itemName] = item_data
+    return item_data
 }
 
 return this
