12a4ad0029b8fc3ee0ba88d2a38ef0ac6e78020c
Erich Birngruber <erich.birngruber@gmi.oeaw.ac.at>
2019-08-21 11:05:17 +0200

    typo is CAPS

--- vars/onepass.groovy
+++ vars/onepass.groovy
@@ -132,8 +132,8 @@ def raw(String itemName, String vault = null) {
     def vault_param = vault ? "--vault=${vault}" : ""
     def item_raw = sh label: "onepass", script: "op get item --session=${onePassToken} ${itemName} ${vault_param}", returnStdout: true
 
-    echo "raw item data: ${item_raw}"
-    def item_data = readJson text: "${item_raw}"
+    //echo "raw item data: ${item_raw}"
+    def item_data = readJSON text: item_raw
 
 //    itemCache[itemName] = item_data
     return item_data
